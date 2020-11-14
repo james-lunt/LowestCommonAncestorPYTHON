721cf2999d347c342f1a5db6da493d9f1afd62c1
James Lunt <50912137+james-lunt@users.noreply.github.com>
2020-11-14 14:28:03 +0000

    LCA DAG
    Lowest common ancestor that works for a directed acyclic graph.

--- LCATest.py
+++ LCATest.py
@@ -1,22 +1,42 @@
 import unittest
-from lca import Node,findLCA,findPath
+from LCA import Node, lca
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        root = Node(1)
-        root.left = Node(2)
-        root.right = Node(3)
-        root.left.left = Node(4)
-        root.left.right = Node(5)
-        root.right.left = Node(6)
-        root.right.right = Node(7)
-        self.assertEqual(findLCA(root,4,5),2)
-        self.assertEqual(findLCA(root, 4, 6),1)
-        self.assertEqual(findLCA(root, 3, 4),1)
-        self.assertEqual(findLCA(root, 2, 4),2)
+class TestLca(unittest.TestCase):
+
+    def test_node1_3(self):
+        self.assertEqual(lca.dagLCA(lca.root, lca.root.key, lca.r3.key).key, 1)
+
+    def test_node_None(self):
+        self.assertEqual(lca.dagLCA(None, 1, 3), None)
+
+    def test_node3_1(self):
+        self.assertEqual(lca.dagLCA(lca.root, 3, 1).key, 1)
+
+    def test_node2_3(self):
+        self.assertEqual(lca.dagLCA(lca.root, lca.r2, lca.r3), 1)
+
+    def test_node5_4(self):
+        self.assertEqual(lca.dagLCA(lca.root, lca.r5, lca.r4), 3)
+
+    def test_node4_3(self):
+        self.assertEqual(lca.dagLCA(lca.root, lca.r4, lca.r3), 1)
+
+    def test_node5_2(self):
+        self.assertEqual(lca.dagLCA(lca.root, lca.r5, lca.r2), 1)
+
+    def test_node6_5(self):
+        self.assertEqual(lca.dagLCA(lca.root, lca.r6, lca.r5), 4)
+
+    def test_node6_3(self):
+        self.assertEqual(lca.dagLCA(lca.root, lca.r6, lca.r3), 1)
+
+    def test_node6_4(self):
+        self.assertEqual(lca.dagLCA(lca.root, lca.r6, lca.r4), 4)
+
+    def test_node_same(self):
+        self.assertEqual(lca.dagLCA(lca.root, lca.r6, lca.r6), 6)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
-
